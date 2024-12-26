fed90273e5ec7982c0d073f2a26461f3cc76fcd0
1D3R4X <glorytosaintiderax@mail.ru>
2024-12-26 14:32:40 +0300

    Update functions.js

--- netlify/functions.js
+++ netlify/functions.js
@@ -1,9 +1,16 @@
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
-
-server.use(middlewares);
-server.use(router);
-
+const jsonServer = require('json-server'); 
+const server = jsonServer.create(); 
+const router = jsonServer.router('db.json'); 
+const middlewares = jsonServer.defaults(); 
+ 
+server.use((req, res, next) => { 
+  res.removeHeader("Access-Control-Allow-Origin"); 
+  res.removeHeader("Access-Control-Allow-Headers"); 
+  res.removeHeader("Access-Control-Allow-Methods"); 
+  next(); 
+}); 
+ 
+server.use(middlewares); 
+server.use(router); 
+ 
 exports.handler = serverless(server);
